57e09ffc8d81fdbb353f14590c13452c81013d23
Kholkin <kholkins@mail.ru>
2020-03-17 20:31:22 +0300

    design layout

--- app/src/main/java/com/example/musicshop/MainActivity.java
+++ app/src/main/java/com/example/musicshop/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.musicshop;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.TextureView;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void setText(View view) {
+        TextView newText = findViewById(R.id.quantityNumberTextView);
+        newText.setText("Hello");
+        newText.setBackgroundResource(R.color.colorAccent);
+        newText.setTextSize(30);
+    }
+
+    public void increaseQuantity(View view) {
+    }
+
+
 }
