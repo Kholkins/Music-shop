b7db56ed0af173663ff1c08c01c29eddfd20ced6
Kholkin <kholkins@mail.ru>
2020-03-19 19:08:07 +0300

    intent to Order Activity

--- app/src/main/java/com/example/musicshop/OrderActivity.java
+++ app/src/main/java/com/example/musicshop/OrderActivity.java
@@ -2,13 +2,22 @@ package com.example.musicshop;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class OrderActivity extends AppCompatActivity {
+
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
+
+        Intent orderIntent = getIntent();
+        String userName = orderIntent.getStringExtra("userName");
+        textView = (TextView)findViewById(R.id.userNameTextView);
+        textView.setText(userName);
     }
 }
