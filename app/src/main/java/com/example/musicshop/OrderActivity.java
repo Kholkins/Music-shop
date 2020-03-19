0801228364b0f9f4f98daaf36f14f921733e234e
Kholkin <kholkins@mail.ru>
2020-03-19 21:39:26 +0300

    intent to Order Activity

--- app/src/main/java/com/example/musicshop/OrderActivity.java
+++ app/src/main/java/com/example/musicshop/OrderActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Window;
 import android.widget.TextView;
 
 public class OrderActivity extends AppCompatActivity {
@@ -15,12 +16,15 @@ public class OrderActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
 
+       setTitle("You order");
+
         Intent orderIntent = getIntent();
         String userName = orderIntent.getStringExtra("userName");
         String goodsName = orderIntent.getStringExtra("goodsName");
         int quantity = orderIntent.getIntExtra("quantity", 0);
         double orderPrice = orderIntent.getDoubleExtra("orderPrice", 0);
         textView = (TextView)findViewById(R.id.userNameTextView);
-        textView.setText(userName + " "+ goodsName + " "+ quantity + " "+ orderPrice);
+        textView.setText("Customer name: "+userName + "\nGoods name: "+ goodsName + "\nQuantity: "+ quantity + "\nPrice: "+ orderPrice/quantity + "\nOder price: " + orderPrice);
+
     }
 }
