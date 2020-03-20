84260d8109db0e60f9a16d1e95f73dfb953b9421
Kholkin <kholkins@mail.ru>
2020-03-20 10:03:16 +0300

    SubmitOrder

--- app/src/main/java/com/example/musicshop/OrderActivity.java
+++ app/src/main/java/com/example/musicshop/OrderActivity.java
@@ -11,8 +11,9 @@ import android.widget.TextView;
 public class OrderActivity extends AppCompatActivity {
 
     private TextView textView;
-    String[] addresses;
-    String subject;
+    String[] addresses = {"kholkins@mail.ru"};
+    String subject = "Order of Music shop";
+    String emailText;
 
 
     @Override
@@ -28,7 +29,8 @@ public class OrderActivity extends AppCompatActivity {
         int quantity = orderIntent.getIntExtra("quantity", 0);
         double orderPrice = orderIntent.getDoubleExtra("orderPrice", 0);
         textView = (TextView)findViewById(R.id.userNameTextView);
-        textView.setText("Customer name: "+userName + "\nGoods name: "+ goodsName + "\nQuantity: "+ quantity + "\nPrice: "+ orderPrice/quantity + "\nOder price: " + orderPrice);
+        emailText = "Customer name: "+userName + "\nGoods name: "+ goodsName + "\nQuantity: "+ quantity + "\nPrice: "+ orderPrice/quantity + "\nOder price: " + orderPrice;
+        textView.setText(emailText);
 
     }
 
@@ -37,6 +39,7 @@ public class OrderActivity extends AppCompatActivity {
         intent.setData(Uri.parse("mailto:")); // only email apps should handle this
         intent.putExtra(Intent.EXTRA_EMAIL, addresses);
         intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        intent.putExtra(Intent.EXTRA_TEXT, emailText);
         if (intent.resolveActivity(getPackageManager()) != null) {
             startActivity(intent);
         }
